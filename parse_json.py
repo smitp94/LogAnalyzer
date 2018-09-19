02e5f0edfbc29ab62872f4b00b3819c8491573f9
smitp94 <smitp94@gmail.com>
2018-09-19 13:25:35 -0700

    Scatter Plot Time distribution

--- parse_json.py
+++ parse_json.py
@@ -1,7 +1,22 @@
 import json
 from pprint import pprint
+from plot import plot_scatter
+
 
 with open('data/test.json') as f:
     data = json.load(f)
 
-pprint(data[0])
+x = []
+y = []
+for i in range(len(data)):
+    # pprint(data[i]['durations']['response'])
+    # pprint(data[i]['durations']['total'])
+    if data[i]['durations']['total'] is not None: #and data[i]['durations']['total'] < 100000: # to remove one big outlier
+        x.append(data[i]['durations']['response'])
+        y.append(data[i]['durations']['total'])
+print(len(x))
+print(len(y))
+
+plot_scatter(x, y)
+
+# date, date-time
--- plot.py
+++ plot.py
@@ -1,2 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
+
+def plot_scatter(x, y):
+    x = [0 if i is None else i for i in x]
+    y = [0 if i is None else i for i in y]
+
+    plt.scatter(x, y, s=np.pi*3, c=(0,0.5,0.5), alpha=0.5)
+    plt.title('Scatter plot total time vs response time')
+    plt.xlabel('response')
+    plt.ylabel('total')
+    plt.show()
