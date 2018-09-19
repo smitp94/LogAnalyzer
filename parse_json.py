59f849a5a864607cf7d049eea22732f467e17382
smitp94 <smitp94@gmail.com>
2018-09-19 14:12:10 -0700

    Scan and parse for all Log files in directory

--- parse_json.py
+++ parse_json.py
@@ -1,22 +1,29 @@
 import json
 from pprint import pprint
 from plot import plot_scatter
+import os
 
 
-with open('data/test.json') as f:
-    data = json.load(f)
+def parse():
+    plots = {}
+    for file in os.listdir('data'):
+            filename = os.fsdecode(file)
+            if filename.endswith(".chlsj"):
+                file = open("data/" + filename, "r")
+                data = json.load(file)
+                plots[filename] = {x: '', y: ''}
+                x = []
+                y = []
 
-x = []
-y = []
-for i in range(len(data)):
-    # pprint(data[i]['durations']['response'])
-    # pprint(data[i]['durations']['total'])
-    if data[i]['durations']['total'] is not None: #and data[i]['durations']['total'] < 100000: # to remove one big outlier
-        x.append(data[i]['durations']['response'])
-        y.append(data[i]['durations']['total'])
-print(len(x))
-print(len(y))
+                for i in range(len(data)):
+                    # pprint(data[i]['durations']['response'])
 
-plot_scatter(x, y)
+                    if data[i]['durations']['total'] is not None and data[i]['durations']['total'] < 100000: # to remove one big outlier
+                        x.append(data[i]['durations']['response'])
+                        y.append(data[i]['durations']['total'])
 
+                plot_scatter(x, y)
+
+
+parse()
 # date, date-time
--- plot.py
+++ plot.py
@@ -5,9 +5,20 @@ import numpy as np
 def plot_scatter(x, y):
     x = [0 if i is None else i for i in x]
     y = [0 if i is None else i for i in y]
-
-    plt.scatter(x, y, s=np.pi*3, c=(0,0.5,0.5), alpha=0.5)
+    color = (0,0.5,0.5)
+    plt.scatter(x, y, s=np.pi*3, c=color, alpha=0.5)
     plt.title('Scatter plot total time vs response time')
     plt.xlabel('response')
     plt.ylabel('total')
+    plt.show()
+
+
+def plot_drive(plots):
+    color = (0, 0.5, 0.5)
+    for i in range(len(plots)):
+        plt.subplot(len(plots), 1, i)
+        plt.scatter(plots[i].x, plots[i].y, s=np.pi * 3, c=color, alpha=0.5)
+        plt.title(plots[i])
+        plt.xlabel('response')
+        plt.ylabel('total')
     plt.show()
